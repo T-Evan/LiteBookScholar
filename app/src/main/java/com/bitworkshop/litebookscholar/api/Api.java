e082ace5df268e2e93e1358b8ab03e70515eb82a
Aid913 <785375121@qq.com>
2016-10-26 17:14:04 +0800

    Update Api.java

--- app/src/main/java/com/bitworkshop/litebookscholar/api/Api.java
+++ app/src/main/java/com/bitworkshop/litebookscholar/api/Api.java
@@ -5,6 +5,5 @@ package com.bitworkshop.litebookscholar.api;
  */
 
 public class Api {
-//    public static final String BASE_URL = "http://123.207.42.17:2333/Login/LibraryFetch.php?/";
-    public static final String LOGIC_BASE_URL = "http://123.207.42.17:8090/";
+    public static final String LOGIC_BASE_URL = "";
 }
